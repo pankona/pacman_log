c9b81848882e664c7c762f50f7ec70272c4dfb39
pankona <yosuke.akatsuka@gmail.com>
2016-02-23 22:42:29 +0900

    update script to do git add and commit automatically

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo pacman -Syu | tee `date +"%Y%m%d_%H%M"`.txt
+DATE=`date +"%Y.%m.%d"`
+FILE=`date +"%Y%m%d_%H%M"`.txt
+
+sudo pacman -Syu | tee $FILE
+git add $FILE && git commit -m "add log $DATE"
